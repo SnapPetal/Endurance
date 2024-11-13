ed79f3d078b27af1507f0f9fc9faa5e6e9c1420a
thonbecker <me@thonbecker.com>
2024-11-12 19:16:04 -0600

    feat: remove stuff

--- src/main/java/com/thonbecker/endurance/configuration/SecurityConfiguration.java
+++ src/main/java/com/thonbecker/endurance/configuration/SecurityConfiguration.java
@@ -1,61 +1,28 @@
 package com.thonbecker.endurance.configuration;
 
-import java.util.List;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.ClientRegistrations;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@AllArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        String issuerUri = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_DZ8a8Cqaw";
-
-        List<ClientRegistration> clientRegistrations = List.of(
-                ClientRegistrations.fromOidcIssuerLocation(issuerUri)
-                        .registrationId("global")
-                        .clientId("7hrr4q4t8j5nha0gm9s0cu0lvj")
-                        .clientSecret("gi4jc8md2p1h2cpqu7puvt89pk9rvh320djhm1lkm41o90ks440")
-                        .redirectUri("https://global.thonbecker.solutions/login/oauth2/code/global")
-                        .clientName("Global App Client")
-                        .scope("openid")
-                        .build(),
-                ClientRegistrations.fromOidcIssuerLocation(issuerUri)
-                        .registrationId("endurance")
-                        .clientId("5uetsd87a3cd3so07omns5bi1q")
-                        .clientSecret("sksa59sd4qhsm6s2gqgjhl8qtoe730k6ho2u6leqrh95iglu6sl")
-                        .redirectUri("https://endurance.thonbecker.solutions/login/oauth2/code/endurance")
-                        .clientName("Endurance App Client")
-                        .scope("openid")
-                        .build(),
-                ClientRegistrations.fromOidcIssuerLocation(issuerUri)
-                        .registrationId("local")
-                        .clientId("e580lipkj7npukhk3o7j2tahp")
-                        .clientSecret("d0ditdrkj0cr52de8hnj8t84103movk6hmfqf1etbarerhtospf")
-                        .redirectUri("http://localhost:8080/login/oauth2/code/local")
-                        .clientName("Local Development")
-                        .scope("openid")
-                        .build());
-        return new InMemoryClientRegistrationRepository(clientRegistrations);
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(Customizer.withDefaults())
+        return http.csrf(Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
                 .authorizeHttpRequests(authz -> authz.requestMatchers("/", "/login/**", "/actuator/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
-                .oauth2Login(oauth2 -> oauth2.loginPage("/login/sso"));
-        return http.build();
+                .build();
     }
 }
