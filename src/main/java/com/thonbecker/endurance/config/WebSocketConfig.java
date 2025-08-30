1c0cb7e7c1fa097042d53a2a22560909e1b4e36e
thonbecker <thon.becker@gmail.com>
2025-08-30 18:34:52 -0500

    chore: code clean up

--- src/main/java/com/thonbecker/endurance/config/WebSocketConfig.java
+++ src/main/java/com/thonbecker/endurance/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.thonbecker.endurance.configuration;
+package com.thonbecker.endurance.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
